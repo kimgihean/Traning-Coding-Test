24c97c78503155e8d6145d9eeb64b31a0b42e3a9
kimgihean <rlgus2738@naver.com>
2022-12-31 16:21:04 +0900

    .

--- src/_1946.java
+++ src/_1946.java
@@ -1,7 +1,18 @@
-
+import java.io.*;
 
 public class _1946 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /*
+        1. 서류심사, 면접성적을 기준으로 sorting
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+
+
+    }
+
+    public void Solution() {
 
     }
 }
