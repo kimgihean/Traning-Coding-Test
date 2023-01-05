6c261bb3b58923f42eff3e04432b5691a61064b9
kimgihean <rlgus2738@naver.com>
2023-01-05 18:24:02 +0900

    1463

---
+++ src/_1463.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class _1463 {
+
+    static int dp[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+        dp = new int[num+1];
+
+        System.out.println(Solution(num));
+    }
+
+    private static int Solution(int num) {
+
+        return 0;
+    }
+}
